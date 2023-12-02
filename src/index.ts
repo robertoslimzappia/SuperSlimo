e66d2852fbd4ccf202a31a19bdf8355f4f3dbea2
robertoslimzappia <robertoslimzappia@gmail.com>
2023-12-02 14:09:15 +0100

    Idle Hero Co-authored-by: Bruno Sirianni <DarkHarlock@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-document.body.innerHTML = `<h1>Hello TypeScript finalmente!</h1>`;
+import * as PIXI from 'pixi.js';
+import spritesheetData from './assets/spritesheet.json';
+import spritesheetTexture from './assets/spritesheet.png';
+
+const pixiOptions = {
+    background: '#1099bb', // Configuriamo una schermata azzurrina
+    resizeTo: window // Tutta la finestra browser
+};
+const app = new PIXI.Application<HTMLCanvasElement>(pixiOptions); // creo pixi application
+
+document.body.appendChild(app.view);
+
+loadGameSpriteSheet();
+
+async function loadGameSpriteSheet() {
+    const parteGrafica = await PIXI.Assets.load(spritesheetTexture);
+    const spriteSheet = new PIXI.Spritesheet<typeof spritesheetData>(parteGrafica, spritesheetData);
+    await spriteSheet.parse();
+
+    const hero = new PIXI.AnimatedSprite(spriteSheet.animations['hero/idle']);
+    app.stage.addChild(hero);
+    hero.animationSpeed = 20 / 60;
+    hero.scale.set(4);
+    hero.play();
+}
